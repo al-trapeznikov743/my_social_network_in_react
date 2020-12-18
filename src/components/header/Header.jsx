5cc79197cb888416d63abf33a42037eeed545053
al-trapeznikov <al.trapeznikov.743@gmail.com>
2020-12-18 23:53:53 +0500

    start project

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import nasaLogo from '../../icons/NASA_logo.svg'
+import searchIcon from '../../icons/search.svg'
+import headerPhoto from '../../img/Rick.jpg'
+import styles from '../../styles/Header.module.sass'
 
 const Header = () => {
-    return <div>
+    return  <div className={styles.header}>
+                <img src={nasaLogo} alt="logo" className={styles.logo}/>
+                <div className={styles.tools}>
+                    <img src={searchIcon} alt="img" className={styles.search_img}/>
+                    <input placeholder='Search' type='text' className={styles.top_search}/>
+                    <div className={styles.welcome}>
+                        <span>Hey, John!</span>
+                        <img src={headerPhoto} alt="img" className={styles.pr_photo}/>
+                    </div>
+                </div>
     </div>
 }
 
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Profile from '../profile/Profile'
+import Sidebar from './Sidebar'
+import styles from '../../styles/Main.module.sass'
 
 const Main = () => {
-    return <div>
+    return <div className={styles.main}>
+        <Sidebar />
+        <Profile />
     </div>
 }
 
